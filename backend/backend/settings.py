8ef3e8f9c5272d01739f39102088e00584410658
BHUVANESHWARAN B <92454566+BBHUVANESHWARAN@users.noreply.github.com>
2025-06-05 16:46:12 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,3 +126,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings.py
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
